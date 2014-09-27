a348266ea07f475b437f7262112d47b61245aa0e
Abhinav <abhi.pat93@gmail.com>
2014-09-27 11:22:38 +0530

    second commit
    with recursive crawling!! :D :D

--- new1/spiders/flipk_spider.py
+++ new1/spiders/flipk_spider.py
@@ -1,5 +1,6 @@
 import scrapy
-
+from urlparse import urljoin
+from scrapy.http import Request
 from new1.items import DmozItem
 
 class Dmoz1Spider(scrapy.Spider):
@@ -7,20 +8,38 @@ class Dmoz1Spider(scrapy.Spider):
     allowed_domains = ["flipkart.com"]
     start_urls = [
         "http://www.flipkart.com/google-nexus-5/product-reviews/ITMDZKKQHQXYC64R"
+    
     ]
-
+    
     def parse(self, response):
         #for sel in response.xpath('//ul/li'):
         item = DmozItem()
+        items = []
         #item['title'] = sel.xpath('a/text()').extract()
         item['link'] = response.xpath("//a[@class='nav_bar_link']/@href").extract()
         bigs = []
         bigs = response.xpath("//a[@class='nav_bar_link']/@href")
         print "--------------------------------------------------"
         print bigs
+        print "--------------------------------------------------"
         for big in item['link']:
             print "--------------------------------------------------"
             print big
             #item['desc'] = sel.xpath('text()').extract()
-            item['title'] = big.xpath("//span[@class='review-text']").extract()
-            yield item
+            #link = item['link'][0] if len(item['link']) else None
+            if big:
+                yield Request(urljoin(response.url, big),
+                    callback=self.parse_link,
+                    errback=lambda _: item,
+                    meta=dict(item=item),
+                    )
+                print "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
+                print item
+                print "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
+            else:
+                yield items
+            
+    def parse_link(self, response):
+        item = response.meta.get('item')
+        item['title'] = response.xpath("//span[@class='review-text']").extract()
+        return item
